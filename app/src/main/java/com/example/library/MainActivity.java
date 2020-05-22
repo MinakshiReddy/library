eb41ee37baf5cc8a7b43ecabd2d3a6e0dd72be60
MinakshiReddy <studentminakshee@gmail.com>
2020-05-22 22:38:32 +0530

    button and edittext invisible on click

--- app/src/main/java/com/example/library/MainActivity.java
+++ app/src/main/java/com/example/library/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.library;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button login;
+        final EditText text;
+        login = findViewById(R.id.login);
+        text=findViewById(R.id.text);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, text.getText().toString(), Toast.LENGTH_SHORT).show();
+                login.setEnabled(false);
+                login.setVisibility(View.INVISIBLE);
+                text.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
