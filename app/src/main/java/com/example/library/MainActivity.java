2a3bd211ba8614c9f8027f3b150a522a80a34ea9
MinakshiReddy <studentminakshee@gmail.com>
2020-05-23 10:47:59 +0530

    update image onclick

--- app/src/main/java/com/example/library/MainActivity.java
+++ app/src/main/java/com/example/library/MainActivity.java
@@ -6,33 +6,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button login;
-        final EditText text;
-        final TextView hello;
-        hello = findViewById(R.id.hello);
-        hello.setVisibility(View.INVISIBLE);
-
         login = findViewById(R.id.login);
-        text=findViewById(R.id.text);
-
-        login.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, text.getText().toString(), Toast.LENGTH_SHORT).show();
+            public void onClick(View v)
+            {
+                ImageView image = (ImageView) findViewById(R.id.pic);
+                image.setImageResource(R.drawable.staryellow);
+                Toast.makeText(MainActivity.this, "image changed", Toast.LENGTH_SHORT).show();
                 login.setEnabled(false);
-                login.setVisibility(View.INVISIBLE);
-                text.setVisibility(View.INVISIBLE);
-                hello.setVisibility(View.VISIBLE);
             }
         });
-    }
+    };
 }
